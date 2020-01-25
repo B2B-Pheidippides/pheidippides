ad8938990248d49c5a2b663c44480d2ed5ed4243
Liam Adams <liam1540@googlemail.com>
2020-01-25 17:12:32 +0000

    Temp hack to place comms bar correctly at the bottom of the page.

--- comps/CommonLayout.js
+++ comps/CommonLayout.js
@@ -3,10 +3,30 @@ import Nav from './Nav';
 import CommsBar from './CommsBar';
 
 const Layout = props => (
-  <div className="uk-container">
-    <div className="uk-position-top"><Nav/></div>
-    {props.children}
-    <div className="uk-position-bottom"><CommsBar/></div>
+  <div className="page-container">
+    <div className="content-wrap">
+      <div className="uk-section"><Nav/></div>
+      {props.children}
+    </div>
+    <div className="comms-bar"><CommsBar/></div>
+    
+    <style jsx>{`
+        .page-container {
+          position: relative;
+          min-height: 100vh;
+        }
+  
+        .content-wrap {
+          padding-bottom: 40px;
+        }
+  
+        .comms-bar {
+          position: absolute;
+          bottom: 0;
+          width: 100%;
+          height: 40px;  
+        }
+      `}</style>
   </div>
 );
 
