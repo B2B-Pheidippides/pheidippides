eed12a64f9df21688e514dcda4b378f3de75f5df
Liam Adams <liam1540@googlemail.com>
2020-01-25 15:25:23 +0000

    Remove old styling.

--- comps/CommonLayout.js
+++ comps/CommonLayout.js
@@ -2,24 +2,11 @@
 import Nav from './Nav';
 import CommsBar from './CommsBar';
 
-const layoutStyle = {
-  margin: 0,
-  padding: 0,
-};
-
 const Layout = props => (
-  <div class="uk-container" style={layoutStyle}>
+  <div class="uk-container">
     <div class="uk-position-top"><Nav/></div>
     {props.children}
     <div class="uk-position-bottom"><CommsBar/></div>
-    <style jsx global>
-        {`
-            body {
-                margin: 0;
-                padding: 0;
-            }
-        `}
-    </style>
   </div>
 );
 
