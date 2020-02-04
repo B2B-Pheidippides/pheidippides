06b9a1edc0ce450ad5cafc0e39aa72ad5cf23c6d
Liam Adams <liam1540@googlemail.com>
2020-02-04 19:09:39 +0000

    Add image query to index page and use static layout.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,26 @@
 
-import Layout from '../comps/CommonLayout';
+import LayoutStatic from '../comps/LayoutStatic';
+import Query from "../comps/CMSQuery";
+import IMAGES_QUERY from "../apollo/cmsQueries/image";
 
 export default function Index() {
   return (
-    <div>
-      <Layout>
-        <p>Welcome to our home page</p>
-      </Layout>
-    </div>
+    <Query query={IMAGES_QUERY} id={1}>
+    { 
+      ({data: images}) => {
+        return ( 
+          <LayoutStatic>
+            <div data-uk-height-viewport="expand: true">
+              <div className="uk-height-1-1 uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light"
+                   data-src={process.env.API_URL + images.images[0].image.url}
+                   data-srcset={process.env.API_URL + images.images[0].image.url} data-uk-img>
+                <h1>Background Image</h1>
+              </div>
+            </div>
+          </LayoutStatic>
+        );
+      }
+    }
+    </Query>
   );
 }
