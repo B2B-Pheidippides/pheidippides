46eabbe66066b399c9de5c32c51179625969ecf5
Liam Adams <liam1540@googlemail.com>
2020-02-04 20:51:42 +0000

    Add a countdown to home page.

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,54 @@
 import LayoutStatic from '../comps/LayoutStatic';
 import Query from "../comps/CMSQuery";
 import IMAGES_QUERY from "../apollo/cmsQueries/image";
+import EVENT_QUERY from '../apollo/cmsQueries/event';
 
 export default function Index() {
   return (
     <Query query={IMAGES_QUERY} id={1}>
     { 
       ({data: images}) => {
-        return ( 
-          <LayoutStatic>
-            <div data-uk-height-viewport="expand: true">
-              <div className="uk-height-1-1 uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light"
-                   data-src={process.env.API_URL + images.images[0].image.url}
-                   data-srcset={process.env.API_URL + images.images[0].image.url} data-uk-img>
-                <h1>Background Image</h1>
-              </div>
-            </div>
-          </LayoutStatic>
+        return (
+          <div>
+          <div className="uk-background-cover"
+                data-src={process.env.API_URL + images.images[0].image.url}
+                data-srcset={process.env.API_URL + images.images[0].image.url} data-uk-img>
+            <LayoutStatic>
+                <div data-uk-height-viewport="expand: true">
+                <div className="uk-container uk-position-center uk-text-center">
+                  <h1 className="uk-light uk-text-italic">B2B Starting Soon!</h1>
+                  <hr/>
+                  <Query query={EVENT_QUERY} id={1}>
+                    {
+                      ({data: events}) => {
+                        return (
+                          <div className="uk-flex uk-flex-center uk-light" uk-countdown={"date: " + events.events[0].dtg}>
+                            <div>
+                              <span className="uk-countdown-number uk-countdown-days"></span>
+                              <div className="uk-countdown-label uk-margin-small uk-text-center uk-visible@s">Days</div>
+                            </div>
+                            <span className="uk-countdown-separator uk-padding-small uk-padding-remove-top uk-padding-remove-bottom">:</span>
+                            <div>
+                              <span className="uk-countdown-number uk-countdown-hours"></span>
+                              <div className="uk-countdown-label uk-margin-small uk-text-center uk-visible@s">Hours</div>
+                            </div>
+                            <span className="uk-countdown-separator uk-padding-small uk-padding-remove-top uk-padding-remove-bottom">:</span>
+                            <div>
+                              <span className="uk-countdown-number uk-countdown-minutes"></span>
+                              <div className="uk-countdown-label uk-margin-small uk-text-center uk-visible@s">Minutes</div>
+                            </div>
+                          </div>
+                        );
+                      }
+                    }
+
+                  </Query>
+
+                </div>
+                </div>
+            </LayoutStatic>
+          </div>
+          </div>
         );
       }
     }
